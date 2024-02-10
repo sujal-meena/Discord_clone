451e9243989cdd095687d4ffc6a447e0fe541bdf
sujal-meena <sujalmeena250122@gmail.com>
2024-02-11 00:53:22 +0530

    first changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
     <p className="text-3xl">
       hello learning
+      
     </p>
+
+    <Button>Click me</Button>
+    </div>
   );
 }
